60a88f1ac1f3627fbddcee9a7c5cf64c55ecae38
galcyurio <galcyurio@gmail.com>
2019-07-18 02:36:27 +0900

    composing suspending functions 에서 Lazily started async 까지 추가

--- src/main/kotlin/com/github/galcyurio/composing_suspending_functions.kt
+++ src/main/kotlin/com/github/galcyurio/composing_suspending_functions.kt
@@ -1,5 +1,6 @@
 package com.github.galcyurio
 
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
@@ -57,14 +58,50 @@ suspend fun doSomethingUsefulTwo(): Int {
  * 아래 예제는 두 개의 코루틴을 동시에 실행하기 때문에 두 배 빠릅니다.
  * 코루틴의 동시성은 항상 명시적이라는 걸 기억하세요.
  * */
+//fun main() = runBlocking {
+//    val time = measureTimeMillis {
+//        val one = async { doSomethingUsefulOne() }
+//        val two = async { doSomethingUsefulTwo() }
+//        println("The answer is ${one.await() + two.await()}")
+//    }
+//    println("Completed in $time ms")
+//}
+// 실행결과
+// The answer is 42
+// Completed in 1030 ms
+
+
+/**
+ * ## Lazily started async
+ *
+ * `start` 매개변수에 [CoroutineStart.LAZY]를 넘겨서 [async]에서 지연시킬 수 있습니다.
+ * 이렇게하면 `await` 또는 `start` 함수가 호출되었을 경우와 같이 결과값이 필요할 때 코루틴이 시작됩니다.
+ * */
 fun main() = runBlocking {
     val time = measureTimeMillis {
-        val one = async { doSomethingUsefulOne() }
-        val two = async { doSomethingUsefulTwo() }
+        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
+        // some computation
+        one.start()
+        two.start()
         println("The answer is ${one.await() + two.await()}")
     }
     println("Completed in $time ms")
 }
 // 실행결과
 // The answer is 42
-// Completed in 1030 ms
+// Completed in 1033 ms
+
+/**
+ * 두 개의 코루틴들이 정의되어 있지만 이전 예제처럼 실행되지는 않았습니다.
+ * 그리고 `start` 함수를 통해 프로그래머에게 제어권이 주어집니다.
+ * 위 예제에서는 먼저 `one`을 시작하고 그 다음 `two`를 시작한 뒤
+ * 각각의 코루틴에 대해서 `await` 했습니다.
+ *
+ * 여기서 중요한 점은 우리가 만약 `start`를 제외하고 `println`에서 `await`만 했다면
+ * 코루틴은 순차적으로 동작할 것입니다.
+ * 이것은 지연 동작의 의도적인 사용방법이 아닙니다.
+ *
+ * `async (start = CoroutineStart.LAZY)`의 사용은 값 계산에
+ * suspending function이 포함된 경우 `lazy` 함수를 대체합니다.
+ * */
