8f293f1fcdc031ab7acbdc411c9f9d5990188529
galcyurio <galcyurio@gmail.com>
2020-06-16 23:46:13 +0900

    다른 파일과 충돌하지 않도록 private로 선언

--- src/main/kotlin/3_continuation.kt
+++ src/main/kotlin/3_continuation.kt
@@ -1,15 +1,15 @@
-class Item
-class Token
-class Post
+private class Item
+private class Token
+private class Post
 
-suspend fun postItem(item: Item) {
+private suspend fun postItem(item: Item) {
     val token = requestToken()
     val post = createPost(token, item)
     processPost(post)
 }
 
-suspend fun requestToken(): Token = Token()
-suspend fun createPost(token: Token, item: Item): Post = Post()
-fun processPost(post: Post) {
+private suspend fun requestToken(): Token = Token()
+private suspend fun createPost(token: Token, item: Item): Post = Post()
+private fun processPost(post: Post) {
     println("processPost")
 }
